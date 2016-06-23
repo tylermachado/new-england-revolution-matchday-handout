cfb706c03b3758e4a444ab6f945e261c4c8a1e74
tyler-machado <tyler.machado@hbr.org>
2016-06-23 16:38:54 -0400

    svg adjustments and styles

--- js/app.js
+++ js/app.js
@@ -3,62 +3,76 @@ var	width = 730,
 	height = width,
 	square = width/n,
 	squarepadding = 0,
-	radius = 5;
+	radius = 10;
 
 var xscale = d3.scale.linear()
-		  .domain([0, n])
-		  .range([0, -2*Math.PI]);              // used to scale node index to x position
+		  .domain([0,n])
+		  .range([0, -2*Math.PI]);              
 
-	    var yscale = d3.scale.linear()
-		  .domain([0, n])
+var yscale = d3.scale.linear()
+		  .domain([0,n])
 		  .range([0, 2*Math.PI]);   
 
 var color = d3.scale.linear()
   .domain([-10, 0, 10])
   .range(["#ca0020","#f7f7f7","#0571b0"]);
 
-  var svg = d3.select("body").append("svg")
+var svg = d3.select("body").append("svg")
 		.attr("width", width)
 		.attr("height", height);
 
 
+// var textRevs = svg.append("text")
+// 		.text("REVS")
+// 		.attr({
+// 			"text-anchor":"middle",
+// 			x: width/2,
+// 			y: width/2,
+// 			class: "clubname"
+// 		});
+
+// var textCrew = svg.append("text")
+// 		.text("CREW")
+// 		.attr({
+// 			"text-anchor":"middle",
+// 			x: width/2,
+// 			y: width/2 + 120,
+// 			class: "clubname"
+// 		});
+
+// var textTop = svg.append("text")
+// 		.text("JULY 26, 2016")
+// 		.attr({
+// 			"text-anchor":"middle",
+// 			x: width/2,
+// 			y: 30,
+// 			class: "date"
+// 		});
+
+// var textBottom = svg.append("text")
+// 		.text("GILLETTE STADIUM, FOXBORO, MA")
+// 		.attr({
+// 			"text-anchor":"middle",
+// 			x: width/2,
+// 			y: width-5,
+// 			class: "date"
+// 		});
+
+
+
 function createDots(data) {
-	// console.log(data);
-	
-    var dots = svg.selectAll("rect")
+ 	var games = svg.selectAll("rect")
 			.data(data.data)
 			.enter().append("g");
 
-		dots.selectAll("circle")
+	var dots = games.selectAll("circle")
 	  		.data(function(d) { return d3.range(d); })
 	  		.enter().append("circle")
 			.attr("data-goals", function(d) {return d;})
 			.attr("r", radius)
-			.attr("cx", function(d, i, j) { return (width/2) + (((i*data.dir*10) + data.radius) * Math.sin(xscale(j))) })
-			.attr("cy", function(d, i, j) { return (width/2) + (((i*data.dir*10) + data.radius) * Math.cos(yscale(j))) })
-			.style("fill", function(d, i) { return data.color; });
-
-	// dots.selectAll("circle")
- //  		.data(function(d) { return d3.range(d); })
- //  		.enter()
- //  		.append("circle")
-	// 		.attr("data-goals", function(d) {return d;})
-	// 		.attr("r", radius)
-	// 		.attr("cx", function(d, i, j) { return (j % n) * (square+squarepadding) + radius } )
-	// 		.attr("cy", function(d, i, j) { 
-	// 			if (data == revs) {
-	// 				return (height/2) - (radius) - (radius*2*d); 
-	// 			} else {
-	// 				return (height/2) + (radius) + (radius*2*d); 
-	// 			}
-	// 		 } )
-	// 		.style("fill", function(d, i, j) { 
-	// 			if (data == revs) {
-	// 				return "#0A2141"; 
-	// 			} else {
-	// 				return "#FFDB00"; 
-	// 			}
-	// 		} );
+			.attr("cx", function(d, i, j) { return (width/2) + (((i*data.dir*radius*2) + data.radius) * -Math.sin(xscale(j))) })
+			.attr("cy", function(d, i, j) { return (width/2) + (((i*data.dir*radius*2) + data.radius) * -Math.cos(yscale(j))) })
+			.style("fill", data.color);
 }
 
 createDots(revs);
