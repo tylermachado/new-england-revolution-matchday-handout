8f5ae93df24dbc1819f2b93452bdb5c5759c98d7
tyler-machado <tyler.machado@hbr.org>
2016-06-24 10:24:34 -0400

    change to arc diagram

--- js/app.js
+++ js/app.js
@@ -3,15 +3,15 @@ var	width = 730,
 	height = width,
 	square = width/n,
 	squarepadding = 0,
-	radius = 10;
+	radius = 5;
 
 var xscale = d3.scale.linear()
-		  .domain([0,n])
-		  .range([0, -2*Math.PI]);              
+		  .domain([0,n-1])
+		  .range([-0.5*Math.PI, 0.5*Math.PI]);              
 
 var yscale = d3.scale.linear()
-		  .domain([0,n])
-		  .range([0, 2*Math.PI]);   
+		  .domain([0,n-1])
+		  .range([-0.5*Math.PI, 0.5*Math.PI]);   
 
 var color = d3.scale.linear()
   .domain([-10, 0, 10])
@@ -22,41 +22,41 @@ var svg = d3.select("body").append("svg")
 		.attr("height", height);
 
 
-var textRevs = svg.append("text")
-		.text("REVS")
-		.attr({
-			"text-anchor":"middle",
-			x: width/2,
-			y: width/2,
-			class: "clubname"
-		});
+// var textRevs = svg.append("text")
+// 		.text("REVS")
+// 		.attr({
+// 			"text-anchor":"middle",
+// 			x: width/2,
+// 			y: width/2,
+// 			class: "clubname"
+// 		});
 
-var textCrew = svg.append("text")
-		.text("CREW")
-		.attr({
-			"text-anchor":"middle",
-			x: width/2,
-			y: width/2 + 120,
-			class: "clubname"
-		});
+// var textCrew = svg.append("text")
+// 		.text("CREW")
+// 		.attr({
+// 			"text-anchor":"middle",
+// 			x: width/2,
+// 			y: width/2 + 120,
+// 			class: "clubname"
+// 		});
 
-var textTop = svg.append("text")
-		.text("JULY 26, 2016")
-		.attr({
-			"text-anchor":"middle",
-			x: width/2,
-			y: 30,
-			class: "date"
-		});
+// var textTop = svg.append("text")
+// 		.text("JULY 26, 2016")
+// 		.attr({
+// 			"text-anchor":"middle",
+// 			x: width/2,
+// 			y: 30,
+// 			class: "date"
+// 		});
 
-var textBottom = svg.append("text")
-		.text("GILLETTE STADIUM, FOXBORO, MA")
-		.attr({
-			"text-anchor":"middle",
-			x: width/2,
-			y: width-5,
-			class: "date"
-		});
+// var textBottom = svg.append("text")
+// 		.text("GILLETTE STADIUM, FOXBORO, MA")
+// 		.attr({
+// 			"text-anchor":"middle",
+// 			x: width/2,
+// 			y: width-5,
+// 			class: "date"
+// 		});
 
 
 
@@ -70,8 +70,8 @@ function createDots(data) {
 	  		.enter().append("circle")
 			.attr("data-goals", function(d) {return d;})
 			.attr("r", radius)
-			.attr("cx", function(d, i, j) { return (width/2) + (((i*data.dir*radius*2) + data.radius) * -Math.sin(xscale(j))) })
-			.attr("cy", function(d, i, j) { return (width/2) + (((i*data.dir*radius*2) + data.radius) * -Math.cos(yscale(j))) })
+			.attr("cx", function(d, i, j) { return (width/2) + (((i*data.dir*radius*2) + data.radius) * Math.sin(xscale(j))) })
+			.attr("cy", function(d, i, j) { return (width-10) + (((i*data.dir*radius*2) + data.radius) * -Math.cos(yscale(j))) })
 			.style("fill", data.color);
 }
 
